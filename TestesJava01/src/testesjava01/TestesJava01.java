2e43514526eba9d2c062a9f7ca0b97145e323a8f
Joao-Victor-Souza <sjoaosouza01@gmail.com>
2023-02-24 19:03:43 -0300

    Primeira tentativa de calcular delta com valores aleatórios

--- TestesJava01/src/testesjava01/TestesJava01.java
+++ TestesJava01/src/testesjava01/TestesJava01.java
@@ -15,42 +15,20 @@ public class TestesJava01 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        //usando Math.round eu consigo converter um double para um integer.
-        //isso porque eu não estou convertendo diretamente (o que é pribido)
-        //mas sim arredondando e aplicando.
+         double A = 0+ Math.random() * (20-0);
+         double valorA = Math.round(A);
         
+        double p = 0+ Math.random() * (1-0);
+        double valorP = Math.round(p);
         
-        //usar no código do projeto do gênio!!!!!!!
-        /*double doublevalue = 3.3;
-        int intvalue =(int) Math.round(doublevalue);
-        //System.out.println(intvalue);
-        
-        if(intvalue == 3){
-            System.out.println(intvalue);
-        } else{ 
-            System.out.println("nao");
-        }
-        */
-        /*
-        int A = 10;
-        int B = 20;
-        int C = 7;
-        
-        double delta = Math.pow(B, 2) - 4*A*C;
-        
-        System.out.println(delta);
-        
-        //System.out.println("km\u00B2");
-        */
-        double delta = -10;
-        
-        if(delta>0){
-            System.out.println("Maior");
-        }else if(delta == 0){
-            System.out.println("igual");
+       // System.out.println(valorP);
+        if(valorP == 0){
+            System.out.println(valorA * -1);
         }else{
-            System.out.println("menor");
+            System.out.println(valorA);
         }
+        
+        //System.out.println(valorA *-1);
         
     };
     
